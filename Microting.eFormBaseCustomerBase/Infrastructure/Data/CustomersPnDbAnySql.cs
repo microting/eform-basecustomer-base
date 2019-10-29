296e78270eeccb612c3189229153e47d7786e0e5
René Schultz Madsen <rm@microting.dk>
2019-10-29 08:39:56 +0100

    Adding the new PluginGroupPermissionVersions.

--- Microting.eFormBaseCustomerBase/Infrastructure/Data/CustomersPnDbAnySql.cs
+++ Microting.eFormBaseCustomerBase/Infrastructure/Data/CustomersPnDbAnySql.cs
@@ -21,6 +21,7 @@ namespace Microting.eFormBaseCustomerBase.Infrastructure.Data
         public DbSet<PluginConfigurationValueVersion> PluginConfigurationValueVersions { get; set; }
         public DbSet<PluginPermission> PluginPermissions { get; set; }
         public DbSet<PluginGroupPermission> PluginGroupPermissions { get; set; }
+        public DbSet<PluginGroupPermissionVersion> PluginGroupPermissionVersions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
