97c3bf34bbc1727d07aef46845e6122b7e5ce8a8
René Schultz Madsen <rm@microting.dk>
2019-11-11 06:33:01 +0100

    Adding CrmId to customer.

--- Microting.eFormBaseCustomerBase/Infrastructure/Data/Factories/CustomersPnContextFactory.cs
+++ Microting.eFormBaseCustomerBase/Infrastructure/Data/Factories/CustomersPnContextFactory.cs
@@ -9,10 +9,6 @@ namespace Microting.eFormBaseCustomerBase.Infrastructure.Data.Factories
     {
         public CustomersPnDbAnySql CreateDbContext(string[] args)
         {
-            //args = new[]
-            //    {"host=localhost;Database=customers-pl;Uid=root;Pwd=111111;port=3306;Convert Zero Datetime = true;SslMode=none;PersistSecurityInfo=true;"};
-            //args = new[]
-            //    {"Data Source=.\\SQLEXPRESS;Database=customers-pl;Integrated Security=True"};
             var optionsBuilder = new DbContextOptionsBuilder<CustomersPnDbAnySql>();
             if (args.Any())
             {
---
+++ Microting.eFormBaseCustomerBase/Migrations/20191111053159_AddingCrmIdToCustomer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.eFormBaseCustomerBase.Migrations
+{
+    public partial class AddingCrmIdToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CrmId",
+                table: "CustomerVersions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CrmId",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CrmId",
+                table: "CustomerVersions");
+
+            migrationBuilder.DropColumn(
+                name: "CrmId",
+                table: "Customers");
+        }
+    }
+}
